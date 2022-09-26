b709f450e7974b3237fe1479a54ba61933567cbd
rmdms <remy.benjamin.dumas@gmail.com>
2022-09-26 10:31:58 -0400

    Test twitter

--- src/pages/api/test/index.jsx
+++ src/pages/api/test/index.jsx
@@ -26,4 +26,18 @@ export default async function handle(req, res) {
   //   headers: { 'Content-Type': 'application/json' },
   //   body: JSON.stringify(datas),
   // });
+  var T = new Twit({
+    consumer_key: "MAmF4GwxtnB8CJGu3GaYOSw9p",
+    consumer_secret: "nz4YayEkFmEaEXbmZ19L7kdodOl3DVNlbHdO4KNNQuFTdEw7m1",
+    access_token: "dDRyWXJ2VUZIeVFWVVU1aWdnVUU6MTpjaQ",
+    access_token_secret: "BJGkYob8gDiu_6x45GW5SJ1aaqrHUO-qaUmFG-IdtFZIIPEd3m"
+  });
+  function gotData(err, data) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].text);
+    }
+    res.json(data)
+  }
+  T.get('search/tweets', { q: 'JavaScript', count: 10 }, gotData);
 }
