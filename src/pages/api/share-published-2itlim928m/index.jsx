dbc1b378c6eaa5ae78a9571a53ccc6dd87bcdee8
rmdms <remy.benjamin.dumas@gmail.com>
2022-09-26 11:43:42 -0400

    Test twitter

--- src/pages/api/share-published-2itlim928m/index.jsx
+++ src/pages/api/share-published-2itlim928m/index.jsx
@@ -1,29 +1,46 @@
-import Twit from "twit";
+// import Twit from "twit";
 
-export default async function handle(req, res) {
-  // const datas = req.body.entry
-  // const model = req.body.model
-  // let urlDiscord = undefined
-  // switch (model) {
-  //   case "spotlights-post":
-  //     urlDiscord = ""
-  //     break;
-  //   case "insights-post":
-  //     urlDiscord = ""
-  //     break;
-  //   case "resources-post":
-  //     urlDiscord = ""
-  //     break;
-  //   case "projects-post":
-  //     urlDiscord = ""
-  //     break;
-  //   default:
-  //     return null
-  //     break;
-  // }
-  // await fetch(urlDiscord, {
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/json' },
-  //   body: JSON.stringify(datas),
-  // });
+// export default async function handle(req, res) {
+//   // const datas = req.body.entry
+//   // const model = req.body.model
+//   // let urlDiscord = undefined
+//   // switch (model) {
+//   //   case "spotlights-post":
+//   //     urlDiscord = ""
+//   //     break;
+//   //   case "insights-post":
+//   //     urlDiscord = ""
+//   //     break;
+//   //   case "resources-post":
+//   //     urlDiscord = ""
+//   //     break;
+//   //   case "projects-post":
+//   //     urlDiscord = ""
+//   //     break;
+//   //   default:
+//   //     return null
+//   //     break;
+//   // }
+//   // await fetch(urlDiscord, {
+//   //   method: 'POST',
+//   //   headers: { 'Content-Type': 'application/json' },
+//   //   body: JSON.stringify(datas),
+//   // });
+// }
+
+
+export async function getServerSideProps({ req, res }) {
+  return {
+    props: {
+      body: req.body,
+    }
+  };
+}
+
+export default function Test(props) {
+  console.log(props)
+  return (
+    <>
+    </>
+  );
 }
