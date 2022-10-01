8506254919b0e8ecbc45dcec1b7aa65bd6cc9716
rmdms <remy.benjamin.dumas@gmail.com>
2022-10-01 11:44:48 -0400

    Test

--- src/pages/spotlights/[slug]/index.jsx
+++ src/pages/spotlights/[slug]/index.jsx
@@ -9,7 +9,7 @@ export default function Spotlight({ spotlight, ...props }) {
   console.log(spotlight)
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta key='description' name='description' content={ spotlight.description } />
         <meta key='og-title' property='og:title' content={ spotlight.title } />
         <meta key='og-description' property='og:description' content={ spotlight.description } />
@@ -18,7 +18,7 @@ export default function Spotlight({ spotlight, ...props }) {
         <meta key='tw-description' name='twitter:description' content={ spotlight.description } />
         <meta key='tw-image' name='twitter:image:src' content={ spotlight.image.url } />
       </Head>
-      <SpotlightTemplate spotlight={ spotlight } />
+      <SpotlightTemplate spotlight={ spotlight } /> */}
     </>
   )
 }
@@ -32,14 +32,14 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({params, ...context}) {
-  const spotlight = await getPostSpotlight(params.slug)
+  // const spotlight = await getPostSpotlight(params.slug)
   // if (!spotlight) {
   //   return {
   //     notFound: true,
   //   }
   // }
   return {
-    props: { spotlight },
+    props: { spotlight: "ok" },
     revalidate: 1
   }
 }
