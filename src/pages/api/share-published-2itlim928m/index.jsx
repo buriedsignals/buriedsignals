44bf0ad6c7be9cc17e07a2ce1e9b95eabdfabdaa
rmdms <remy.benjamin.dumas@gmail.com>
2022-10-03 12:25:27 -0400

    Update tweet

--- src/pages/api/share-published-2itlim928m/index.jsx
+++ src/pages/api/share-published-2itlim928m/index.jsx
@@ -22,8 +22,8 @@ export default async function handle(req, res) {
     })
     let link = await responseLink.json()
     link = link.link
-    let template = `Spotlight on ${ datas.Title } | ${ datas.Categories[0].Title }\n\n•\n\n${ link }\n\n#narrativevisualisation #informationdesign`
-    const templateExtra = `Spotlight on ${ datas.Title } | ${ datas.Categories[0].Title }\n\n•${ description }\n\n${ link }\n\n#narrativevisualisation #informationdesign`
+    let template = `Spotlight: ${ datas.Title } | ${ datas.Categories[0].Title }\n\n•\n\n${ link }\n\n#narrativevisualisation #informationdesign`
+    const templateExtra = `Spotlight: ${ datas.Title } | ${ datas.Categories[0].Title }\n\n•${ description }\n\n${ link }\n\n#narrativevisualisation #informationdesign`
     if (template.length + description.length <= 300) {
       template = templateExtra
     }
