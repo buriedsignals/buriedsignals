0203135606a435398f059671cd4ddc6a5bf6e601
Rémy Dumas <remy.benjamin.dumas@gmail.com>
2023-07-18 22:46:26 +0800

    Update

--- src/pages/api/post-update-spotlights-archive-cron-3k4qt81hmr/index.jsx
+++ src/pages/api/post-update-spotlights-archive-cron-3k4qt81hmr/index.jsx
@@ -2,7 +2,9 @@ import { createArchiveSpotlightCron } from "@/middlewares/librairies/posts/spotl
 import axios from "axios";
 
 export default async function handle(req, res) {
+  console.log(req.body)
   const post = req.body.split(',')
+  console.log(post)
   if (post.length == 2) {
     const archiveId = post[0]
     const scheduleId = post[1]
